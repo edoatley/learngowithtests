24dc545d38b8dd027bf8b15bc309bb39ec03801a
edoatleynewday <ed.oatley@newday.co.uk>
2023-07-17 15:28:48 +0100

    first chapter complete

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,16 +2,47 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
 
-// separate the logic from the side effect of printing to the console
-func Hello(name string) string {
+// Also, we can group constants in a block instead of declaring them each on their own line. 
+// It's a good idea to use a line between sets of related constants for readability.
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+
+// The function name starts with a uppercase letter. 
+// In Go, public functions start with a capital letter and private ones start with a lowercase.
+func Hello(name string, language string) string {
+
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+// note the named return value prefix
+//   - It will be assigned the "zero" value. This depends on the type, for example ints are 0 and for strings it is "".
+//   - You can return whatever it's set to by just calling return rather than return prefix.
+//   - This will display in the Go Doc for your function so it can make the intent of your code clearer.
+func greetingPrefix(language string) (prefix string) { 
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		// default in the switch case will be branched to if none of the other case statements match.
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -4,25 +4,36 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Pierre", "French")
+		want := "Bonjour, Pierre"
 		assertCorrectMessage(t, got, want)
 	})
 
 }
 
-
-// In this helper function we've refactored our assertion into a new function. 
-// This reduces duplication and improves readability of our tests. 
+// In this helper function we've refactored our assertion into a new function.
+// This reduces duplication and improves readability of our tests.
 // We need to pass in t *testing.T so that we can tell the test code to fail when we need to.
 //
-// For helper functions, it's a good idea to accept a testing.TB which is an interface that *testing.T and *testing.B both satisfy, 
+// For helper functions, it's a good idea to accept a testing.TB which is an interface that *testing.T and *testing.B both satisfy,
 // so you can call helper functions from a test, or a benchmark (don't worry if words like "interface" mean nothing to you right now, it will be covered later).
 func assertCorrectMessage(t testing.TB, got, want string) {
 	// t.Helper() is needed to tell the test suite that this method is a helper. By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
